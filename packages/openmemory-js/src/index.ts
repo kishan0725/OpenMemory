f83717b44ae21c7149792319f86f7f0b4382e1d9
Kavin-kumar10 <kavinkumar.prof@gmail.com>
2026-01-19 12:59:07 +0530

    feat: Added exports necessary for proxy or custom tool creation

--- packages/openmemory-js/src/index.ts
+++ packages/openmemory-js/src/index.ts
@@ -1,5 +1,41 @@
-
+// Export core functionality for use as a package
 export * from "./core/memory";
-export * from "./server/index";
+// NOTE: Do NOT export server/index as it auto-starts the HTTP server
+// export * from "./server/index";  // Commented out to prevent auto-start
 export * from "./ops/ingest";
 export * as sources from "./sources";
+
+// Export temporal graph types and functions
+export * from "./temporal_graph/types";
+export * from "./temporal_graph/store";
+export * from "./temporal_graph/query";
+
+// Export HSG (semantic memory) functions - used by MCP tools
+export {
+    add_hsg_memory,
+    hsg_query,
+    reinforce_memory,
+    update_memory,
+    sector_configs,
+    sectors,
+} from "./memory/hsg";
+
+// Export HSG types
+export type {
+    hsg_q_result,
+    hsg_mem,
+    sector_cfg,
+    sector_class,
+} from "./memory/hsg";
+
+// Export database query functions - used by MCP tools
+export { q, all_async, vector_store } from "./core/db";
+
+// Export utility functions
+export { j, p } from "./utils";
+
+// Export user summary function
+export { update_user_summary } from "./memory/user_summary";
+
+// Export core types
+export type { mem_row, sector_type } from "./core/types";
